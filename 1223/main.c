0e33a2e696c86cf9405347432520b9f373c45eaa
Nahuel Quintana <47321353+Nahuellauquintana1@users.noreply.github.com>
2019-04-01 17:22:58 -0300

    Ejercicio terminado.
    cuenta con un menu que permite llenar , mostrar, buscar un valor x dentro del array, y mostrar el maximo

--- 1223/main.c
+++ 1223/main.c
@@ -8,11 +8,50 @@ int main()
     int numero[A];
     int maximo;
     int lugar;
-    cargarArray(numero, A);
-    mostrarArray(numero, A);
-    maximo = maximoArray(numero, A);
-    lugar = buscarValor(numero, A, 5);
-    printf("La poscicion de memoria es: %d",lugar);
-    printf("El maximo es: %d", maximo);
+    int buscar;
+    int option;
+    int continuar = 's';
+    do
+    {
+        printf("1.Cargar el array\n");
+        printf("2.Mostrar array\n");
+        printf("3.Mostrar el numero mayor\n");
+        printf("4.Ingresar el valor a buscar dentro del array\n");
+        printf("5.Salir\n");
+        scanf("%d",&option);
+        switch(option)
+        {
+        case 1:
+            cargarArray(numero, A);
+            break;
+        case 2:
+            mostrarArray(numero, A);
+
+            break;
+        case 3:
+            maximo = maximoArray(numero, A);
+            printf("El numero mayor es: %d\n", maximo);
+            break;
+        case 4:
+            printf("Numero a buscar en el array\n");
+            scanf("%d", &buscar);
+            lugar = buscarValor(numero, A, buscar);
+            if(lugar == -1){
+                printf("El valor ingresado no forma parte de este array\n");
+            }else{
+            printf("El numero se encuentra en la poscision: %d del vector\n", lugar);
+            }
+            break;
+        case 5:
+            continuar = 'n';
+            break;
+        default:
+            printf("Ingrese una opcion valida\n");
+            break;
+        }
+        system("pause");
+        system("cls");
+    }
+    while(continuar == 's');
     return 0;
 }
